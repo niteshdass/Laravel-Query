8a9c3b72c5759fd2bb0a3b23a42e5117fcc0d8cb
Nitesh Das <39939643+niteshdass@users.noreply.github.com>
2023-01-09 19:16:14 +0600

    Pagination-show

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,12 @@ use Illuminate\Support\Facades\DB;
 */
 
 Route::get('/', function () {
-
-    $rooms = DB::table('users')->whereJsonContains('meta->skills', 'Laravel')->get();
-    $rooms = DB::table('users')->where('meta->skills', 'Laravel')->get();
+    //! It should use for normal step by step pagination
+    // $rooms = DB::table('users')->paginate(10);
+    //! It should use for infinity loader pagination
+    // $rooms = DB::table('users')->simplePaginate(10);
+    //! Dynamic pagination
+    $rooms = DB::table('users')->paginate($perPage = 10, $columns = ['name'], $pageName = 'user');
     dd($rooms);
 
     return view('welcome');
